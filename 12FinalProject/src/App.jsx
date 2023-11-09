028b997ec987815ff30ecc89cf18bddb319ef4fe
abhishek <abhishekshukla821@gmail.com>
2023-11-09 16:29:32 +0530

    appwrite setup

---
+++ 12FinalProject/src/App.jsx
@@ -0,0 +1,13 @@
+import {  } from 'react'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
